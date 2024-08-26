fac32044fc381a98d35a651c25585c787f1b11f9
unknown <dhymyantt1@gmail.com>
2024-08-26 08:35:46 +0100

    feat:enable-1

--- Utils.jsx
+++ Utils.jsx
@@ -20,10 +20,12 @@ export const generateUserSummary = (formData) => {
   const formattedDateOfBirth = `${day}/${month}/${year}`;
 
   // Construct the summary
-  const summary = `I am ${firstName} ${lastName}, born on ${formattedDateOfBirth}, a resident of ${currentCity}, ${currentState}. Originally from ${hometownCity}, ${hometownState}, I am currently working as an ${occupation}. Standing at ${height} ft/in, I am ${relationshipStatus} and have ${noOfChildren} ${noOfChildren === 1 ? 'child' : 'children'}. I describe myself as more of a ${cityGirlOrCountryGirl.toLowerCase()}, I am applying to the show "Farmer Wants A Wife" because I want ${whyApply.toLowerCase()}.`;
+  const summary = `I am ${firstName} ${lastName}, born on ${formattedDateOfBirth}, a resident of ${currentCity}, ${currentState}. Originally from ${hometownCity}, ${hometownState}, I am currently working as an ${occupation}. Standing at ${height} ft/in, I am ${relationshipStatus} and have ${noOfChildren} ${
+    noOfChildren === 1 ? "child" : "children"
+  }. I describe myself as more of a ${cityGirlOrCountryGirl.toLowerCase()}, I am applying to the show "Farmer Wants A Wife" because I want ${whyApply.toLowerCase()}.`;
 
   return summary;
 };
 
-// export const baseUrl = 'https://genericbackend.onrender.com';
-export const baseUrl = 'http://localhost:8080';
+export const baseUrl = "https://genericbackend.onrender.com";
+// export const baseUrl = 'http://localhost:8080';
